a2f46aa9724e108e4f76418165073eb3a0353d18
zhanghui <zhanghui@rdyork.cn>
2018-01-08 10:45:52 +0800

    Initial Commit

---
+++ Pthread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pthread
+//
+//  Created by 张辉 on 2018/1/8.
+//  Copyright © 2018年 Bo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pthread/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Pthread
+//
+//  Created by 张辉 on 2018/1/8.
+//  Copyright © 2018年 Bo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
