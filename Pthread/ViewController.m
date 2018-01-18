65d3c3391d8ca7bd4012bc4f15e81e5db1677e67
zhanghui <zhanghui@rdyork.cn>
2018-01-18 16:16:38 +0800

    test

--- Pthread/ViewController.m
+++ Pthread/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import <pthread.h>
 @interface ViewController ()
 
 @end
@@ -17,9 +17,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(threadAction:) object:@"zzzz"];//[NSThread performSelector:@selector(zzzz:) withObject:nil afterDelay:<#(NSTimeInterval)#>];
+    [thread start];
+    
+    
+    pthread_t pthread;
+    pthread_create(&pthread, NULL, &pthreadMethod, nil);
+}
+void *pthreadMethod(void *msg){
+    NSLog(@"dddd");
+    return NULL;
+}
+-(void)threadAction:(id)sender{
+    NSLog(@"zzzzzzdddddd%@",sender);
 }
 
-
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    NSThread *thread = [[NSThread alloc] initWithBlock:^{
+        NSLog(@"block is in%@",[NSThread currentThread]);
+        [[NSRunLoop currentRunLoop] run];
+    }];
+    [thread start];
+    
+    [self performSelector:@selector(demoIn:) onThread:thread withObject:nil waitUntilDone:NO];
+}
+-(void)demoIn:(id)sender{
+    NSLog(@"并行");
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
